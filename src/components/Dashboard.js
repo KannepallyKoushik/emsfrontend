35818c5549d021ea2eb185af70ae94037a225b84
koushik <koushik.koushik.kumar@gmail.com>
2021-04-17 20:54:18 +0530

    Conditional Dashboard based on Role completed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import "../App.css";
+import axios from "../axios";
+
+function AdminDash(props) {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <h5>Welcome {props.name}</h5>
+    </div>
+  );
+}
+
+function StudentDash(props) {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      <h5>Welcome {props.name}</h5>
+    </div>
+  );
+}
 
 const Dashboard = ({ setAuth }) => {
+  const [role, setRole] = useState("");
+  const [username, setUsername] = useState("");
+
   const logout = async (e) => {
     e.preventDefault();
     try {
@@ -14,9 +36,47 @@ const Dashboard = ({ setAuth }) => {
     }
   };
 
+  const getData = async () => {
+    try {
+      axios
+        .post(
+          "/dashboard/",
+          { dummybody: "dummy" },
+          {
+            headers: { token: localStorage.token },
+            "Content-type": "application/json",
+          }
+        )
+        .then((res) => {
+          const parseRes = res.data;
+          setRole(parseRes.user_role);
+          setUsername(parseRes.username);
+        })
+        .catch((er) => {
+          console.log(er.response.data);
+        });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function AuthorizationDash(props) {
+    const user_role = props.role;
+    if (user_role === "admin") {
+      return <AdminDash name={username} />;
+    } else if (user_role === "student") {
+      return <StudentDash name={username} />;
+    }
+    return <div></div>;
+  }
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <AuthorizationDash role={role} />
       <button onClick={(e) => logout(e)} className="btn btn-primary">
         Logout
       </button>
